90d349e2d12d4a9d1699e5232fe46ebe3b6a9e06
Douglas Monteles <silvadouglas328@gmail.com>
2022-07-18 19:59:56 -0300

    Create app

---
+++ src/test/java/br/com/fga/bazar/BazarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fga.bazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
